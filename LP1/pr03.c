d55d85131d858cfa4f66c6822968ecdf622be2f4
Tiago Almeida <>
2022-10-18 23:13:13 +0100

    1

--- LP1/pr03.c
+++ LP1/pr03.c
@@ -7,13 +7,17 @@
 int main_pr03() {
     //int x = pow_recursive(2, 5);
     //printf("%d", x);
-    int a[] = {1,4,2,4};
+    int a[] = {30, 41, 42, 18, 24, 25 , 4, 12};
     //int b[] = {1,2,3,9};
-    int size = sizeof(a)/ sizeof(int);
+    int size = sizeof(a) / sizeof(int);
     //int x = int_compare(a, b, size);
     //printf("%d", x);
-    int x = lowest_index_number(a, 4, size);
-    printf("%d",x);
+    //int x = lowest_index_number(a, 4, size);
+    //int x = highest_index_number(a, 4, size);
+    //int x = number_highest_frequency(a,size);
+    //int x = size_greatest_segment_repeated_ints(a, size);
+    int x = count_clumps (a, size);
+    printf("%d", x);
     return 0;
 }
 
@@ -68,9 +72,9 @@ int int_compare(int a[], int b[], int size) {
 * @return - lowest index which contains the integer n
 * e.g. input: ([1, 4, 2, 4], 4, 4); output: 1
 */
-int lowest_index_number(const int v[], int n, int size){
-    for(int i = 0; i < size; i++){
-        if(v[i] == n){
+int lowest_index_number(const int v[], int n, int size) {
+    for (int i = 0; i < size; i++) {
+        if (v[i] == n) {
             return i;
             break;
         }
@@ -86,12 +90,14 @@ int lowest_index_number(const int v[], int n, int size){
 * @return - highest index which contains the integer n
 * e.g. input: ([1, 4, 2, 4], 4, 4); output: 3
 */
-int highest_index_number(int v[], int n, int size){
-   for(int i = 0; i < size; i++){
-        if(v[i] == n){
-            return i;
+int highest_index_number(int v[], int n, int size) {
+    int aux = 0;
+    for (int i = 0; i < size; i++) {
+        if (v[i] == n) {
+            aux = i;
         }
-   } 
+    }
+    return aux;
 }
 
 /**
@@ -102,18 +108,115 @@ int highest_index_number(int v[], int n, int size){
 * e.g. input: ([1, 4, 2, 4], 4); output: 4
 * e.g. input: ([1, 1, 1, 4], 4); output: 1
 */
-int number_highest_frequency(int v[], int size){
+int number_highest_frequency(int v[], int size) {
     int count = 0, maxNumber = 0;
-    for(int i = 0; i < size; i++){
+    for (int i = 0; i < size; i++) {
         count = 1;
-        for(int x = i+1; x < size; x++){
-            if(v[i] == v[j]){
-                count ++;
-            }   
+        for (int x = i + 1; x < size; x++) {
+            if (v[i] == v[x]) {
+                count++;
+            }
         }
-         if(count > maxNumber){
-                maxNumber = v[i];
-         }
+        if (count > maxNumber) {
+            maxNumber = v[i];
+        }
     }
     return maxNumber;
+}
+
+/**
+* get the lenght of the greatest sequence of repeated ints
+* @param v - array to be searched
+* @param size - array size
+* @return - lenght of the greatest sequence
+* e.g. input: ([55, 22, 22, 33, 44, 44, 44, 1, 1], 9); output: 3
+*/
+int size_greatest_segment_repeated_ints(int v[], int size) {
+
+
+    int count = 0, count1 = 0;
+/**------------------------- Parte 1 ----------------------*/
+//Saber tamanho da matriz
+    for (int i = 0; i < size; i++) {
+        int z;
+        for (z = 0; z < i; z++) {
+            if (v[i] == v[z]) {
+                break;
+            }
+        }
+
+        if (i == z) {
+            count1++;
+        }
+    }
+
+    int matrix[count1][2];
+
+    /**------------------------- Parte 2 ----------------------*/
+    //Guardar os numeros diferentes na matriz
+    int z, k = 0;
+    for (int i = 0; i < size; i++) {
+        count = 1;
+
+        for (z = 0; z < i; z++) {
+            if (v[i] == v[z]) {
+                break;
+            }
+        }
+        for (int x = i + 1; x < size; x++) {
+            if (v[i] == v[x]) {
+                count++;
+            }
+        }
+        matrix[k][1] = count;
+        if (i == z && k < count1) {
+            matrix[k][0] = v[i];
+            k++;
+        }
+    }
+/**------------------------- Parte 3 ----------------------*/
+//Saber qual e o maior numero com o maior numero de repeticoes
+    int maior1 = 0, maior2 = 0;
+    for (int x = 0; x < count1; x++) {
+        if (matrix[x][1] > maior1) {
+            maior1 = matrix[x][1];
+        }
+    }
+
+    for (int x = 0; x < count1; x++) {
+        if (matrix[x][1] == maior1) {
+            maior2 = matrix[x][0];
+            if (matrix[x][0] > maior2) {
+                maior2 = matrix[x][0];
+            }
+        }
+    }
+    for (int x = 0; x < count1; x++) {
+
+        if (matrix[x][0] == maior2) {
+            return matrix[x][1];
+        }
+    }
+    return 0;
+}
+
+/**
+* count the number of clumps (a series of 2 or more adjacent equal
+* elements) in a given array
+* @param a - array to be searched
+* @param size - array size
+* @return - number of clumps
+* e.g. input: ([30, 41, 41, 18, 24, 24, 4, 12], 8); output: 2
+* e.g. input: ([30, 41, 42, 18, 24, 24, 24, 12], 8); output: 1
+* e.g. input: ([30, 41, 42, 18, 24, 25 , 4, 12], 8); output: 0
+*/
+int count_clumps (int a[], int size){
+    int count = 0;
+    for(int i = 0; i < size; i++){
+        if(a[i] == a[i+1] && a[i] != a[i + 2]){
+            count++;
+            i++;
+        }
+    }
+    return count;
 }
--- LP1/pr03.h
+++ LP1/pr03.h
@@ -13,4 +13,7 @@ int int_compare(int a[], int b[], int size);
 int lowest_index_number(const int v[], int n, int size);
 int highest_index_number(int v[], int n, int size);
 int number_highest_frequency(int v[], int size);
+int size_greatest_segment_repeated_ints(int v[], int size);
+int count_clumps (int a[], int size);
+
 #endif //INC_2023_PR03_H
